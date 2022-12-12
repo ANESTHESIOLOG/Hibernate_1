362a786a9b5543db9d8049a6bb7418f786908590
zotov <zotovegor952@gmail.com>
2022-12-12 22:58:51 +0300

    commit4

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,6 +2,8 @@ package org.example;
 
 
 import org.example.model.Person;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 /**
@@ -12,5 +14,19 @@ public class App
 {
     public static void main( String[] args ) {
         Configuration configuration = new Configuration().addAnnotatedClass(Person.class);
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.getCurrentSession();
+
+
+        session.beginTransaction();
+
+        Person person = session.get(Person.class, 1);
+        System.out.println(person.getName());
+        System.out.println(person.getAge());
+
+        session.getTransaction().commit();
+
+        sessionFactory.close();
+
     }
 }
