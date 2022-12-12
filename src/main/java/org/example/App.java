4722de2ab5a4e774a99eeab17c24fe96a0f71533
zotov <zotovegor952@gmail.com>
2022-12-12 21:00:35 +0300

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,16 @@
+package org.example;
+
+
+import org.example.model.Person;
+import org.hibernate.cfg.Configuration;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) {
+        Configuration configuration = new Configuration().addAnnotatedClass(Person.class);
+    }
+}
